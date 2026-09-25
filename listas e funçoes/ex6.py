6032166bc99b268574e27207232d89d831b07ba1
Jean Duarte <jeanduartequeiroz@gmail.com>
2026-09-25 09:06:53 -0300

    ex6 concluido

--- listas e funçoes/ex6.py
+++ listas e funçoes/ex6.py
@@ -3,5 +3,18 @@
 # A função deve retornar uma nova lista 
 # contendo apenas os elementos que forem estritamente maiores do que o limite informado.
 
+lista = [10, 25, 3, 40, 15];
+resultado = [];
 # resultado = filtrar_maiores([10, 25, 3, 40, 15], 18)
+
+def filtrarMaiores (lista, lim):
+    listaMaior = [];
+    for x in range (0,len(lista),1):
+        if lista[x] > lim:
+            listaMaior.append(lista[x]);
+    return listaMaior;
+
 # Deve retornar: [25, 40]
+
+resultado = filtrarMaiores(lista, 8);
+print(f"A nova lista fica: {resultado}");
